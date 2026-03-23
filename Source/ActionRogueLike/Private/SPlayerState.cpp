54a97b5d24f235157ddcc351154508861818b6ae
noobwhisperer <noobwhisperer@users.noreply.github.com>
2026-03-22 20:48:51 -0700

    commit : Lecture 15.4 : Assignment 5 - WIP -- Credits HUD Widget working
    	Implemented code and BP changes to get the Credits HUD Widget working

--- Source/ActionRogueLike/Private/SPlayerState.cpp
+++ Source/ActionRogueLike/Private/SPlayerState.cpp
@@ -7,3 +7,15 @@
 ASPlayerState::ASPlayerState()
 {
 }
+
+bool ASPlayerState::AdjustCredits(int32 Amount)
+{
+	int32 NewCredits = Credits + Amount;
+	if (NewCredits > MaxCredits || NewCredits < 0)
+	{
+		return false;
+	}
+	Credits = NewCredits;
+	OnCreditsChanged.Broadcast(this, Credits);
+	return true;
+}
--- Source/ActionRogueLike/Public/SPlayerState.h
+++ Source/ActionRogueLike/Public/SPlayerState.h
@@ -6,6 +6,11 @@
 #include "GameFramework/PlayerState.h"
 #include "SPlayerState.generated.h"
 
+
+
+//TSH - maybe I don't need to pass the player state ... I'm passing the NewCredits amount
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnCreditsChanged, ASPlayerState*, PlayerState, int32, NewCredits);
+
 /**
  * 
  */
@@ -21,10 +26,15 @@ protected:
 	UPROPERTY(EditDefaultsOnly, Category = "Player")
 	int32 MaxCredits = 0;
 
+
+	/// <summary>
+	/// Assignable delegate called any time the Credits amount changes
+	/// </summary>
+	UPROPERTY(BlueprintAssignable)
+	FOnCreditsChanged OnCreditsChanged;
+
 public:
-
 	ASPlayerState();
-
 
 	/// <summary>
 	/// Adjust Credits by the given amount. Returns true if the adjustment was successful, false if it would have put
@@ -32,17 +42,13 @@ public:
 	/// </summary>
 	/// <param name="Amount">Amount to adjust the credits by.</param>
 	/// <returns>True if the adjustment was successful, false otherwise.</returns>
-	bool AdjustCredits(int32 Amount)
-	{
-		int32 NewCredits = Credits + Amount;
-		if (NewCredits > MaxCredits || NewCredits < 0)
-		{
-			return false;
-		}
-		Credits = NewCredits;
-		return true;
-	}
+	bool AdjustCredits(int32 Amount);
 
+
+	UFUNCTION(BlueprintPure, Category = "Player")
 	int32 GetCredits() const { return Credits; }
+
+
+	UFUNCTION(BlueprintPure, Category = "Player")
 	int32 GetMaxCredits() const { return MaxCredits; }
 };
