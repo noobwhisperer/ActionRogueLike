ef0dcfdeaba7c7b4f0d5127f0b1dba72511da371
noobwhisperer <noobwhisperer@users.noreply.github.com>
2026-03-23 17:27:07 -0700

    commit : Lecture 16.4 : Improve physics impulse on bot ragdolls
    Lecture 16.4 : Improve physics impulse on bot ragdolls
    
    	- I already improved this, but there's a couple minor changes worth grabbing from Tom
    	- I'm keeping the "vertical" component that I added
    	- I'm keeping the tunable CVar for impulse magnitude multiplier

--- Source/ActionRogueLike/Private/SGameplayFunctionLibrary.cpp
+++ Source/ActionRogueLike/Private/SGameplayFunctionLibrary.cpp
@@ -7,6 +7,7 @@
 
 
 static TAutoConsoleVariable<float> CVarDeathImpulseMultiplier(TEXT("roguelike.DeathImpulseMultiplier"), 2000.0f, TEXT("Global multiplier for damage taken."), ECVF_Cheat);
+static TAutoConsoleVariable<float> CVarDeathImpulseZ(TEXT("roguelike.DeathImpulseZ"), 2.0f, TEXT("Additional Z-component added to death impulse direction."), ECVF_Cheat);
 
 bool USGameplayFunctionLibrary::ApplyDamage(AActor* InstigatorActor, AActor* TargetActor, int32 DamageAmount)
 {
@@ -24,26 +25,19 @@ bool USGameplayFunctionLibrary::ApplyDirectionalDamage(AActor* InstigatorActor, 
 	{
 		UPrimitiveComponent* HitComp = HitResult.GetComponent();
 
-		//debugging - note: still might be a bit of a nasty window where it's possible to hit this ... e.g. player kills bot at same moment play dies?
-		//	might be better to clean this up and send in the "Shot Direction" computed at projectile spawn time instead of trying to
-		//	figure it out by referencing through a bunch of pointers to things that may not exist.
-		//
-		//
-		// if(InstigatorActor == nullptr)
-		// {
-		// 	UE_LOG(LogTemp, Warning, TEXT("Unlucky!"));
-		// }
-
 		if(HitComp && InstigatorActor && HitComp->IsSimulatingPhysics(HitResult.BoneName))
 		{
-			FVector ShotDirection = HitResult.ImpactPoint - InstigatorActor->GetActorLocation();
-			ShotDirection.Normalize();
-
-			// add a little vertical "lift"
-			ShotDirection.Z = 1.5f;
+			FVector ShotDirection = HitResult.TraceEnd - HitResult.TraceStart;
 			ShotDirection.Normalize();
 
 			const float DeathImpulseMultiplier = CVarDeathImpulseMultiplier.GetValueOnGameThread();
+			const float DeathImpulseZ = CVarDeathImpulseZ.GetValueOnGameThread();
+
+
+			// add a little vertical "lift"
+			ShotDirection.Z = DeathImpulseZ;
+			ShotDirection.Normalize();
+
 			HitComp->AddImpulseAtLocation(ShotDirection * DamageAmount * DeathImpulseMultiplier, HitResult.ImpactPoint);
 		}
 		return true;
